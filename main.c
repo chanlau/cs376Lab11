6cdefa019060738061b4e570e15ad06869ebf567
Lau <lauc21@vdi-linux-0160.campus.up.edu>
2020-04-22 17:20:33 -0700

    Only prints UWR robot unless int given in argument

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
  *
  */
 #include "robot.h"
-
+#include <ctype.h>
 /**
  * main()
  *
@@ -21,9 +21,17 @@
  */
 int main(int argc, const char * argv[])
 {
+
+if(argc <= 1){
   robotPrintAscii();
-  dalekPrintAscii();
-  robotPrintMessage();
+}
+else if(isdigit(*argv[1])){
+robotPrintAscii();
+dalekPrintAscii();
+robotPrintMessage();
+}
+//  dalekPrintAscii();
+//  robotPrintMessage();
 
   return 0;
 }
